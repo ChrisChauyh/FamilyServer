59a6ccb3fa259304d65713628463cfb0ca1dba6e
Chris <chauyuhin1998@gmail.com>
2023-03-26 05:30:20 -0600

    Added shared.jar support

--- src/test/java/services/EventIDServiceTest.java
+++ src/test/java/services/EventIDServiceTest.java
@@ -11,7 +11,7 @@ import requestAndResult.EventIDResult;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EventIDServiceTest {
 
